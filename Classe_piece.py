f5d5a5f9fa408e53e39d4d83e687866276ae6965
Despxratz <huang.samy@gmail.com>
2026-05-21 20:40:09 +0200

    Enhance Piece class with docstrings and type hints
    Added docstrings and type hints to the Piece class methods.

--- Classe_piece.py
+++ Classe_piece.py
@@ -1,19 +1,32 @@
 from abc import ABC, abstractmethod
 
 class Piece(ABC):
+    """
+    Toutes les pièces doivent hériter de cette classe
+    """
     def __init__(self, position, color):
         self.position = position  # Objet de type Position
         self.color = color        # 0 pour Blanc, 1 pour Noir
 
     @abstractmethod
-    def isValidMove(self, newPosition, board):
+    def isValidMove(self, newPosition, board) -> bool:
         """
-        Cette méthode doit être remplie par chaque pièce.
+        Cette méthode doit être remplie par chaque pièce. 
+        Elle vérifie si le déplcement vers newPosition est légal.
         Elle doit renvoyer True si le mouvement est autorisé, sinon False.
         """
         pass
 
     @abstractmethod
-    def __str__(self):
+    def __str__(self) -> str:
         """Renvoie la lettre de la pièce (Q, R, B...)"""
         pass
+
+#test
+if __name__ == "__main__":
+    try:
+        p = Piece(None, 0)
+        print("Erreur : J'ai créé une pièce abstraite.")
+    except TypeError as e:
+        print("Test réussi, Impossible de créer une pièce générique.")
+        print(f"Message de Python : {e}")
