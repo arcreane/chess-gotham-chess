25d050c895360f623bb0c07df81a12b4d378a608
zhuzitong1026-create <zhuzitong1026@gmail.com>
2026-05-22 09:34:34 +0200

    feat: Update initPayers() with player.py and aiplayer.py

--- chess.py
+++ chess.py
@@ -1,3 +1,5 @@
+from player import Player, chooseColor
+from aiplayer import AIPlayer
 class Chess:
   def __init__(self, board=None, players=None):
       self.board = board
@@ -5,7 +7,16 @@ class Chess:
       self.currentPlayer =None
   
   def initPlayers(self):
-      pass
+    """Initialise les deux joueurs de la partie"""
+    name = input("Entrez votre nom : ")
+    human_color = chooseColor()
+    human_player = Player(name, human_color)
+    ai_player = AIPlayer(human_color)
+    self.players = [human_player, ai_player]
+    if human_color == 0:
+      self.currentPlayer = human_player
+    else:
+      self.currentPlayer = ai_player
 
   def displayBoard(self):
       print(self.board)
