c5dc2359dcef55f3fcfdc970ad8b07c663706321
zhuzitong1026-create <zhuzitong1026@gmail.com>
2026-05-22 08:53:54 +0200

    feat: Update code aiplayer.py

--- aiplayer.py
+++ aiplayer.py
@@ -1,3 +1,21 @@
 import random
 from player import Player
-"""class AIPlayer(Player):"""
+class AIPlayer(Player):
+  """Classe qui represente un joueur controle par l'ordinateur"""
+  def __init__(self, name, color):
+    """Initialise un joueur IA
+    name: nom du joueur IA
+    color :0 pour blanc, 1 pour noir"""
+    super().__init__(name, color)
+  def askMove(self):
+    """Genere un mouvement aleatoire"""
+    pieces = ["K", "Q", "B", "N", "R", "P"]
+    columns = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    rows = [1, 2, 3, 4, 5, 6, 7, 8]
+    piece = random.choice(pieces)
+    start_column = random.choice(columns)
+    start_row = random.choice(rows)
+    end_column = random.choice(columns)
+    end_row = random.choice(rows)
+    move = piece + start_column + str(start_row) + " " + end_column + str(end_move)
+    return move
