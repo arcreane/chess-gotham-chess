2b93fd203055fc1e37c8a9f5f1e36d3212f3dced
zhuzitong1026-create <zhuzitong1026@gmail.com>
2026-05-22 09:25:01 +0200

    fix: complete player.py classe

--- player.py
+++ player.py
@@ -1,21 +1,25 @@
 class Player:
-  """Initialise un joueur
-  name: nom du joueur
-  color: 0 pour blanc, 1 pour noir"""
-  self.name = name
-  self.color = color
-def askMove(self):
-  """Demande au joueur de saisir son prochain mouvement
-  Exemple de format attendu:
-  Ra1 a4"""
-  move = input("Entrez votre mouvement: ")
-  return move
+  """Classe qui represente un jour d'echecs"""
+  def __init__(self, name, color):
+    """Initialise un joueur
+    name: nom du joueur
+    color: 0 pour blanc, 1 pour noir"""
+    self.name = name
+    self.color = color
+  def askMove(self):
+    """Demande au joueur de saisir son prochain mouvement
+    Exemple de format attendu:
+    Ra1 a4"""
+    move = input("Entrez votre mouvement: ")
+    return move
 def chooseColor():
   """Demande au joueur de choisir la couleur"""
   color = input("Choisissez votre couleur(blanc/noir) : ")
+  color = color.lower().strip()
   while color != "blanc" and color != "noir":
     print("Couleur invalide")
     color = input("Choisissez votre couleur (blanc/noir) : ")
+    color = color.lower().strip()
   if color == "blanc":
     return 0
   else:
