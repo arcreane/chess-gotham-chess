a55f417d0ef05658690a16be2cfa22a866abe0e9
zhuzitong1026-create <zhuzitong1026@gmail.com>
2026-05-22 08:44:10 +0200

    Update code player.py

--- player.py
+++ player.py
@@ -1,1 +1,12 @@
-"""class Player:"""
+class Player:
+  """Initialise un joueur
+  name: nom du joueur
+  color: 0 pour blanc, 1 pour noir"""
+  self.name = name
+  self.color = color
+def askMove(self):
+  """Demande au joueur de saisir son prochain mouvement
+  Exemple de format attendu:
+  Ra1 a4"""
+  move = input("Entrez votre mouvement: ")
+  return move
